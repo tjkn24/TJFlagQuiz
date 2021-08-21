4cb4ab21ca933c02609f1d36cc8fc720fd8997ac
tjkn24 <tjkn24@gmail.com>
2021-08-21 19:46:14 +0700

    blur splash screen background image

---
+++ app/src/main/java/com/example/tjflagquiz/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tjflagquiz
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
