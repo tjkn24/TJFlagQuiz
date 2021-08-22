aa860a9dda82c9c69761e94d65631da3cefa4fbc
tjkn24 <tjkn24@gmail.com>
2021-08-22 21:08:05 +0700

    beginning of inputting questions

--- app/src/main/java/com/example/tjflagquiz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/tjflagquiz/QuizQuestionsActivity.kt
@@ -3,9 +3,9 @@ package com.example.tjflagquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_quiz_questions)
     }
 }
--- app/src/main/java/com/example/tjflagquiz/SplashScreenActivity.kt
+++ app/src/main/java/com/example/tjflagquiz/SplashScreenActivity.kt
@@ -1,11 +1,27 @@
 package com.example.tjflagquiz
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        btn_start.setOnClickListener {
+            if (et_name.text.toString().isEmpty()) {
+                    Toast.makeText(this, "Please Enter Your Name", Toast.LENGTH_LONG).show()}
+                    else {
+                        val intent = Intent(this, QuizQuestionsActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+
+
+        }
     }
 }
