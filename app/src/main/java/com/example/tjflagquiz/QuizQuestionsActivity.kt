1b489768acce2a1d1ce6ea91cfedc04919fe34ca
tjkn24 <tjkn24@gmail.com>
2021-08-23 13:09:36 +0700

    start working on progressbar code

--- app/src/main/java/com/example/tjflagquiz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/tjflagquiz/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.example.tjflagquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,10 @@ class QuizQuestionsActivity : AppCompatActivity() {
 
         val questionList = Constants.getQuestions()
         Log.i("Panjuta", "How many question do we have? ${questionList.size}")
+
+        val currentPosition = 1
+        val question : Question? = questionList[currentPosition - 1]
+        my_progress_bar.progress = currentPosition
+        tv_progress.text = currentPosition.toString() + "/${my_progress_bar.max}"
     }
 }
