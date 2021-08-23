54db49b8b4b7b4e97bca6b37c422d6cb72fac5fe
tjkn24 <tjkn24@gmail.com>
2021-08-23 19:28:10 +0700

    starting onclick

--- app/src/main/java/com/example/tjflagquiz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/tjflagquiz/QuizQuestionsActivity.kt
@@ -1,31 +1,70 @@
 package com.example.tjflagquiz
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_quiz_questions.*
+import org.w3c.dom.Text
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition = 1
+    private lateinit var mQuestionList: ArrayList<Question>
+    private var selectedOptionPosition: Int = 0 //answer tv 1 to 4
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-        val questionList = Constants.getQuestions()
-        Log.i("Panjuta", "How many question do we have? ${questionList.size}")
+        setQuestion()
 
-        var currentPosition = 1
-        var currentQuestion : Question = questionList[currentPosition - 1]
+        tv_option_one.setOnClickListener(this)
+        tv_option_two.setOnClickListener(this)
+        tv_option_three.setOnClickListener(this)
+        tv_option_four.setOnClickListener(this)
+    }
 
-        my_progress_bar.progress = currentPosition
-        tv_progress.text = currentPosition.toString() + "/${my_progress_bar.max}"
+    private fun setQuestion(){
+        setDefaultOptionsView()
+
+        mQuestionList = Constants.getQuestions()
+        var currentQuestion : Question = mQuestionList[mCurrentPosition - 1]
+
+        my_progress_bar.progress = mCurrentPosition
+        tv_progress.text = mCurrentPosition.toString() + "/${my_progress_bar.max}"
         tv_question.text = currentQuestion.question
         iv_image.setImageResource(currentQuestion.image)
         tv_option_one.text = currentQuestion.optionOne
         tv_option_two.text = currentQuestion.optionTwo
         tv_option_three.text = currentQuestion.optionThree
         tv_option_four.text = currentQuestion.optionFour
+    }
+
+    private fun setDefaultOptionsView(){
+        val options = ArrayList<TextView>()
+        options.add(0, tv_option_one)
+        options.add(1, tv_option_two)
+        options.add(2, tv_option_three)
+        options.add(3, tv_option_four)
+
+        for (option in options){
+            option.setTextColor(Color.parseColor("#808080"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.tv_border)
+        }
+    }
+
+    override fun onClick(p0: View?) {
+
+    }
+
+    private fun OptionViewSelected (tv: TextView, optionNumber: Int){
 
     }
 }
